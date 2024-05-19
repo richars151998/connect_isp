8e2388692c60c7e12f107252f6467901d324b644
richars151998 <149031319+richars151998@users.noreply.github.com>
2024-05-19 15:19:20 -0500

    carga inicial

---
+++ Config/Config.php
@@ -0,0 +1,105 @@
+<?php 
+	const BASE_URL = "http://localhost/connect_isp";
+	//const BASE_URL = "https://connect_isp.com/connect_isp";
+
+	//Zona horaria
+	date_default_timezone_set('America/Bogota');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_connect_isp";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Para envío de correo
+	const ENVIRONMENT = 1; // Local: 0, Produccón: 1;
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+	const CURRENCY = "USD";
+
+	//Api PayPal
+	//SANDBOX PAYPAL
+	const URLPAYPAL = "https://api-m.sandbox.paypal.com";
+	const IDCLIENTE = "";
+	const SECRET = "";
+	//LIVE PAYPAL
+	//const URLPAYPAL = "https://api-m.paypal.com";
+	//const IDCLIENTE = "";
+	//const SECRET = "";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "Connect_isp";
+	const EMAIL_REMITENTE = "no-reply@ricardo.com";
+	const NOMBRE_EMPESA = "Connect_isp";
+	const WEB_EMPRESA = "www.connect_isp.com";
+
+	const DESCRIPCION = "La mejor plataforma para gestionar las cajas NAP en tu ISP.";
+	const SHAREDHASH = "Connect_isp";
+
+	//Datos Empresa
+	const DIRECCION = "Calle 72F1#28E1-31";
+	const TELEMPRESA = "+(57) 3207378745";
+	const WHATSAPP = "+573207378745";
+	const EMAIL_EMPRESA = "info@connectisp.com";
+	const EMAIL_PEDIDOS = "info@connectisp.com"; 
+	const EMAIL_SUSCRIPCION = "info@connectisp.com";
+	const EMAIL_CONTACTO = "info@connectisp.com";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+	const CAT_FOOTER = "1,2,3,4,5";
+
+	//Datos para Encriptar / Desencriptar
+	const KEY = 'ricardo';
+	const METHODENCRIPT = "AES-128-ECB";
+
+	//Envío
+	const COSTOENVIO = 5;
+
+	//Módulos
+	const MDASHBOARD = 1;
+	const MUSUARIOS = 2;
+	const MCLIENTES = 3;
+	const MPRODUCTOS = 4;
+	const MPEDIDOS = 5;
+	const MCATEGORIAS = 6;
+	const MSUSCRIPTORES = 7;
+	const MDCONTACTOS = 8;
+	const MDPAGINAS = 9;
+
+	//Páginas
+	const PINICIO = 1;
+	const PTIENDA = 2;
+	const PCARRITO = 3;
+	const PNOSOTROS = 4;
+	const PCONTACTO = 5;
+	const PPREGUNTAS = 6;
+	const PTERMINOS = 7;
+	const PSUCURSALES = 8;
+	const PERROR = 9;
+
+	//Roles
+	const RADMINISTRADOR = 1;
+	const RSUPERVISOR = 2;
+	const RCLIENTES = 3;
+
+	const STATUS = array('Completo','Aprobado','Cancelado','Reembolsado','Pendiente','Entregado');
+
+	//Productos por página
+	const CANTPORDHOME = 8;
+	const PROPORPAGINA = 4;
+	const PROCATEGORIA = 4;
+	const PROBUSCAR = 4;
+
+	//REDES SOCIALES
+	const FACEBOOK = "https://www.facebook.com/connect_isp";
+	const INSTAGRAM = "https://www.instagram.com/connect_isp/";
+	
+
+ ?>
